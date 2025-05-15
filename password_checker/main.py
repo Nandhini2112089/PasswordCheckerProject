aa66c2b33582d20a48c47b8df524669d5143c4c6
Siva Nandhini B <114659701+Nandhini2112089@users.noreply.github.com>
2025-05-15 08:21:19 +0530

    Update main.py

--- password_checker/main.py
+++ password_checker/main.py
@@ -2,4 +2,4 @@ import uvicorn
 from app.core import app 
 
 if __name__ == "__main__":
-    uvicorn.run("app.core:app", host="0.0.0.0", port=8081, reload=True)
+    uvicorn.run("app.core:app", host="0.0.0.0", port=8081)
